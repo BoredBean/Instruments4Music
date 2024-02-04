b161efffcd16252d040fe46d9597b305c7947c41
BoredBean <swordofezio@outlook.com>
2024-02-04 20:33:39 +0800

    Add audio tune interface

--- Instruments4Music/StationaryScript.cs
+++ Instruments4Music/StationaryScript.cs
@@ -3,67 +3,57 @@ using MonoMod.Cil;
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem.HID;
 
 namespace Instruments4Music
 {
     public class StationaryScript : MonoBehaviour
     {
-        private int placeableShipObjectsMask = 67108864;
+        private int _interactableShipObjectsMask = 1 << 9;   //interactableObjects
 
-        private static PlayerControllerB player => GameNetworkManager.Instance?.localPlayerController;
-        private bool _lookingAtInstrument = false;
-
-        private bool IsLookingAtInstrument(out PlaceableShipObject instrumentObj)
+        private static PlayerControllerB Player => GameNetworkManager.Instance?.localPlayerController;
+        
+        private bool IsLookingAtInstrument(out GameObject instrumentObj)
         {
             //When facing the control panel, the X-axis increases forward, the Y-axis increases upwards, and the Z-axis increases to the left.
-            Ray interactRay = new Ray(player.gameplayCamera.transform.position, player.gameplayCamera.transform.forward);
-            RaycastHit hit;
+            var interactRay = new Ray(Player.gameplayCamera.transform.position, Player.gameplayCamera.transform.forward);
 
-            if (player == null || player.isTypingChat || !player.isInHangarShipRoom)
+            if (Player == null || Player.isTypingChat || !Player.isInHangarShipRoom)
             {
                 instrumentObj = default;
                 return false;
             }
-            if ((!Physics.Raycast(interactRay, out hit, player.grabDistance, placeableShipObjectsMask, QueryTriggerInteraction.Ignore) && !Physics.Raycast(player.gameplayCamera.transform.position + Vector3.up * 5f, Vector3.down, out hit, 5f, placeableShipObjectsMask, QueryTriggerInteraction.Ignore)) || !hit.collider.gameObject.CompareTag("PlaceableObject"))
+            if (!Physics.Raycast(interactRay, out var hit, Player.grabDistance, _interactableShipObjectsMask, QueryTriggerInteraction.Ignore) && !Physics.Raycast(Player.gameplayCamera.transform.position + Vector3.up * 5f, Vector3.down, out hit, 5f, interactableShipObjectsMask, QueryTriggerInteraction.Ignore))
             {
                 instrumentObj = default;
                 return false;
             }
-            if (hit.collider.gameObject.layer == 8)
-            {
-                Instruments4MusicPlugin.LOGGER.LogInfo("Layer 8 is: " + hit.transform.name);
-            }
 
-            instrumentObj = hit.collider.gameObject.GetComponent<PlaceableShipObject>();
-
-            Instruments4MusicPlugin.LOGGER.LogInfo("ObjName: " + hit.transform.name);
-            Instruments4MusicPlugin.LOGGER.LogInfo("ObjType: " + hit.transform.gameObject.GetType());
+            instrumentObj = hit.collider.gameObject;
 
             return instrumentObj != null;
         }
 
-        private bool isCoroutineRunning = false;
+        private void LetShowBegins()
+        {
+            if (!IsLookingAtInstrument(out var instrumentObj))
+            {
+                return;
+            }
+            ;
+            ShipAlarmCord horn = instrumentObj.GetComponent<ShipAlarmCord>();
+            if (horn != null)
+            {
+                Instruments4MusicPlugin.LOGGER.LogInfo("Playing ShipAlarm");
+                TunedAudio.RegisterInstrClip("ShipAlarm", 8, horn.hornClose.clip);
+                return;
+            }
+        }
+
         private void Update()
         {
-            if (!isCoroutineRunning)
-            {
-                IsLookingAtInstrument(out PlaceableShipObject instrumentObj);
-                StartCoroutine(DelayedAction());
-            }
 
-            IEnumerator DelayedAction()
-            {
-                isCoroutineRunning = true;
-
-                // 等待1秒钟
-                yield return new WaitForSeconds(0.5f);
-
-                // 在这里执行你想要延时的操作
-                Debug.Log("One second has passed!");
-
-                isCoroutineRunning = false;
-            }
         }
     }
 }
---
+++ Instruments4Music/TunedAudio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Instruments4Music
+{
+    public static class TunedAudio
+    {
+        private const double TuneCoeff = 1.059463f;
+
+        static readonly Dictionary<string, (AudioClip, int)> InstrDictionary = [];
+        static readonly Dictionary<(string, int), AudioSource> TunedDictionary = [];
+
+        public static bool theShowIsOn = false;
+        public static string activeInstrName = "";
+
+        public static void RegisterInstrClip(string instrName, int sourceNoteNumber, AudioClip clip)
+        {
+            if (!InstrDictionary.ContainsKey(instrName))
+            {
+                InstrDictionary.Add(instrName, (clip, sourceNoteNumber));
+            }
+            activeInstrName = instrName;
+            theShowIsOn = true;
+        }
+
+        /* NoteNumber: 0~35 corresponding to the Note names of 3 octaves */
+        public static AudioSource GetTunedAudio(int targetNoteNumber, GameObject gameObject)
+        {
+            if (TunedDictionary.TryGetValue((activeInstrName, targetNoteNumber), out var audioSource))
+            {
+                return audioSource;
+            }
+
+            if (!InstrDictionary.TryGetValue(activeInstrName, out var value))
+            {
+                return null;
+            }
+
+            var clip = value.Item1;
+            var sourceNoteNumber = value.Item2;
+            var power = targetNoteNumber - sourceNoteNumber;
+
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = clip;
+            audioSource.volume = 1.0f;
+            audioSource.pitch = (float)Math.Pow(TuneCoeff, power);
+            TunedDictionary.Add((activeInstrName, targetNoteNumber), audioSource);
+
+            return audioSource;
+        }
+    }
+}
